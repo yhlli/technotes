225d0cd9b097a5b83c5d6300dd893a8a83869a8e
Yiheng Li <yiheng.li@hcl.com>
2024-02-20 14:12:07 -0500

    Ready to deploy

---
+++ src/components/Public.js
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+
+const Public = () => {
+    const content = (
+        <section className="public">
+            <header>
+                <h1>Welcome to <span className="nowrap">Luke's Repairs!</span></h1>
+            </header>
+            <main className="public__main">
+                <p>Located in CT, Luke's Repairs  provides a trained staff ready to meet your tech repair needs.</p>
+                <address className="public__addr">
+                    Luke's Repairs<br />
+                    555 Foo Drive<br />
+                    Foo City, CT 12345<br />
+                    <a href="tel:+15555555555">(555) 555-5555</a>
+                </address>
+                <br />
+                <p>Owner: Luke Li</p>
+            </main>
+            <footer>
+                <Link to="/login">Employee Login</Link>
+            </footer>
+        </section>
+
+    )
+    return content
+}
+export default Public
